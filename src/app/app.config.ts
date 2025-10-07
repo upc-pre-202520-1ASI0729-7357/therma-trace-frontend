791d9b85429174a13c226a8231b9acfae70cca5e
JeanGrandez <jpmle10@hotmail.com>
2025-10-06 19:36:34 -0500

    feat: configure i18n with English and Spanish translations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,9 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { routes } from './app.routes';
 
@@ -7,6 +11,12 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(withFetch()),
+    provideAnimationsAsync(),
+    provideTranslateService({
+      loader: provideTranslateHttpLoader({ prefix: './i18n/', suffix: '.json' }),
+      fallbackLang: 'en'
+    }),
     provideRouter(routes)
   ]
 };
